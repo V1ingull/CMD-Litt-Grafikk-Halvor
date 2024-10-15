c90d09b6cbc5e78bff55fb68d605c3929345814a
Halvor <144103519+V1ingull@users.noreply.github.com>
2024-10-15 19:11:56 +0200

    Add files via upload

---
+++ NTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace NTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = "o";
+            bool Wooloolo = true;
+
+
+            //oppgave:
+            //få dette til å gå evig. ved å bruke en eller annen loop. foreach, while, for
+
+
+            //loop legger til tegn
+            while (true)
+            {
+                Console.WriteLine(symbol);
+                //manipulere symbolet for hver runde
+                if (Wooloolo)
+                {
+                    symbol = symbol + "o";
+
+                    if (symbol.Length == 9)
+                    {
+                        Wooloolo = false;
+                    }
+
+                }
+
+
+                //fjerner tegn
+                else
+                {
+                    symbol = symbol.Remove(symbol.Length - 1, 1);
+
+                    if (symbol.Length == 1)
+
+                    {
+
+                        Wooloolo = true;
+
+                    }
+
+                }
+
+                Thread.Sleep(100);
+
+            }
+
+        }
+    }
+}
